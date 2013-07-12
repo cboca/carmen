68dd204e459ad92aad6cb14dcedad15d6102750c
Josh Barber <roar@jorshasaur.com>
2013-07-11 21:40:22 -0400

    Update main.js
    Cleaned up formatting

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-
 $("document").ready(function() {
  
     $('nav ul li').click(function(){
 		
-		var destination = $(this).data("scroll");
+	var destination = $(this).data("scroll");
 
         $('html, body').animate({
             scrollTop: $("."+destination).offset().top
